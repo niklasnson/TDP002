728e0df9014b295a79fa43455242891cfde469b1
niklas nson <niklasnson@me.com>
2015-08-26 21:15:42 +0200

    adds new 2.4 codes

---
+++ 2_4_1_2a.py
@@ -0,0 +1,3 @@
+import artylib
+#artylib.frame(input("ange en text: "))
+artylib.triangle(input("ange höjd: "))
---
+++ artylib.py
@@ -0,0 +1,14 @@
+def frame(text):
+    text = "* " + text + " *"
+    print("*"*len(text))
+    print(text)
+    print("*"*len(text))
+
+
+def triangle(num):
+    for i in range(1,int(num) + 1):
+        print("*"*i)
+
+
+def flag(num):
+    for i in range
