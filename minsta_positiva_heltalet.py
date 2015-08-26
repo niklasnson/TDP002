2f8dd6a45c988cf910cafa3ff4fb504158f78320
niklas nson <niklasnson@me.com>
2015-08-26 12:27:20 +0200

    adds 1.2.4.1d

--- minsta_positiva_heltalet.py
+++ minsta_positiva_heltalet.py
@@ -1,3 +1,12 @@
-x = 200
+start_tal   = 1
+slut_tal    = 10
+produkt     = 0
 
-print(x%)
+while True:
+    for number in range(start_tal, slut_tal):
+        if start_tal % number != 0:
+            break
+        else:
+            print(number)
+    start_tal = start_tal + 1 # vill inte byta bort den här
+    break # här kan man bryta loopen
