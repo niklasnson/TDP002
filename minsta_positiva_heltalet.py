ffaee5c6f7935afe10ab41a8f57524429f494661
niklas nson <niklasnson@me.com>
2015-08-26 17:22:19 +0200

    solved minsta_possitiva_heltalet

--- minsta_positiva_heltalet.py
+++ minsta_positiva_heltalet.py
@@ -1,12 +1,19 @@
-start_tal   = 1
-slut_tal    = 10
-produkt     = 0
+# Ibland är det inte lika lätt att hitta ett intervall att iterera igenom. Istället får ni bestämma er för ett villkor som bara är sant när ni hittat rätt svar och sedan söka igenom ett mycket stort (ibland oändligt) intervall efter rätt svar.
 
-while True:
-    for number in range(start_tal, slut_tal):
-        if start_tal % number != 0:
-            break
-        else:
-            print(number)
-    start_tal = start_tal + 1 # vill inte byta bort den här
-    break # här kan man bryta loopen
+# Det minsta positiva heltalet som är jämt delbart med siffrorna 1 till och med 10 är talet 2520. Er uppgift är att ta reda på vilket det minsta talet är som är jämt delbart med alla siffror från 1 till och med 13.
+
+# Tips: För att kontrollera ifall ett tal är jämt delbart med ett annat använder ni er av operatorn modulo (%). Ifall resultatet av operationen är noll är talen jämt delbara med varandra.
+
+# defintioner
+is_done     = False
+svar        = 0
+
+
+# main - brutalforce
+while is_done == False:
+    svar = svar + 1
+    is_done = True
+    for number in range(1, 13):
+        if svar % number != 0:
+            is_done = False
+print(svar)
---
+++ summera_alla_primtal_under_1000.py
@@ -0,0 +1,2 @@
+# Ett primtal är ett naturligt tal som är större än 1 och jämt delbart endast med sig själv och med 1. Att avgöra ifall ett tal är ett primtal eller inte är väldigt kostsamt så ni bör därför tänka efter extra noga när ni formulerar er lösning så att ni inte kontrollerar tal som omöjligt kan vara primtal.
+# Summan av alla primtal under 100 är 1060. Er uppgift är att ta reda på summan för samtliga primtal under 1000.
