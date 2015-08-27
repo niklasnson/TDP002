3b3f3b2f1555e99f91395dda0ca6a55543e521fb
niklas nson <niklasnson@me.com>
2015-08-27 09:36:07 +0200

    adds comments and code is working

--- 1_2_2_1b.py
+++ 1_2_2_1b.py
@@ -1,8 +1,11 @@
-i = 1
-x = 513
-summa = 1
+# I denna uppgift ska ni använda er av samma kod som ni skrev i föregående uppgift men nu ska ni göra små ändringar så att ni räknar ut produkten istället för summan.
+# Produkten för alla positiva heltal upp till och med 12 är 479001600, produkten för alla positiva heltal upp till och med 512 kommer således bli ett fruktansvärt stort tal.
 
-while i < x:
-    summa = summa * i
-    i = i + 1
-print(summa)
+lower   = 1
+upper   = 512
+summa   = 1
+
+for i in range(lower, upper + 1):     # eftersom range annars inte kör sista.
+    summa = summa * i                 # summan är lika som summa * akt talk.
+
+print(summa)                          # skriv ut summan
