17ae3ae051894fb3fd95b39f66aca1bc008f409c
niklas nson <niklasnson@me.com>
2015-08-27 09:25:20 +0200

    is done and working

--- 1_2_1_1a.py
+++ 1_2_1_1a.py
@@ -1,7 +1,11 @@
-summa = 0
-i = 0
-x = 513 # input("max tal?: ")
-while i < x:
-    summa = summa + i
-    i = i + 1
+# Ofta när ni utför beräkningar kommer ni i förväg att kunna räkna ut vilket intervall ni måste gå igenom för att komma fram till svaret. Då är en iteration att föredra eftersom ni med säkerhet kan säga att inga yttre faktorer kommer att påverka resultatet.
+# Naturliga tal är de heltal som inte är negativa, det vill säga alla tal från 0 och uppåt. Om ni summerar alla naturliga tal upp till och med 128 får ni summan 8256. Er uppgift är att ta reda på vad summan för alla naturliga tal upp till och med 512 är för något.
+
+summa   = 0
+lower   = 0
+upper   = 512 # input("max tal?: ")
+
+while lower < upper:
+    summa = summa + lower
+    lower = lower + 1
 print(summa)
