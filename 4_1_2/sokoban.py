a19c3f1a9ad6216cba05ed1aeab0d18bc8b4b2e5
Niklas Nson <niklasnson@me.com>
2015-09-29 12:37:44 +0200

    move is working

--- 4_1_2/sokoban.py
+++ 4_1_2/sokoban.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import os
 import sys
-# -- declarations
+
+# -- declarations ----------------------------------------------------- 
 level_map = []
 levels = []
 key_up = 'w' 
@@ -13,7 +16,7 @@ crate='o'
 storage='.'
 player_on_storage='+'
 crate_on_storage='*'
-player='@'
+wall='#'
 
 def sokoban_levels():
     """ Loads all levels filenames into a list """
@@ -24,30 +27,20 @@ def sokoban_levels():
 
 def sokoban_load(filename, row=[]): 
     """ Loads a game map into level_data """
-    with open(filename, encoding='utf-8') as level: 
-        y = 0
+    with open(filename, 'r', encoding='utf-8') as level: 
         for line in level:
-            x = 0 
+            x = 0
             for char in line:
                 if char != " " and char !="\n":
-                    level_map.append([x, y, char])
-                    x = x + 1
-            y = y + 1
-            
-#        for line in level:
-#            x = 0
-#            for char in line:
-#                if char != " " and char !="\n":
-#                    row.append([x, char])
-#                x = x + 1
-#            level_map.append(row)
-#            row=[]
-#    print(level_map)
+                    row.append([x, str(char)])
+                x = x + 1
+            level_map.append(row)
+            row=[]
 
 def sokoban_render_map(level_map=[], dx=0, dy=0):
     """ read the level_data and append floor to empty structures """
-    helper_clear_screen()
-
+    clear_screen()
+    print(level_map)
     for y in level_map:
         for x in y:
             while x[0] > dx:
@@ -65,73 +58,63 @@ def sokoban_cmd():
 
 def sokoban_game(level=0, moves=0, level_complete=False):
     """ This is the main gameloop """
+    sokoban_render_map(level_map)
     while level_complete == False:
         for key in sokoban_cmd():
-            sokoban_render_map(level_map)
-            x, y = find_player()
-            print(x,y)
-            if key == key_up and moveable(x, y - 1): 
-                move(x, y - 1)
-            if key == key_down:
-                pass
-            if key == key_left:
-                pass
-            if key == key_right:
-                pass
+            x, y, player = find_player()
+            if key == key_up and player_moveable(x, y - 1): 
+                move_player(x, y - 1)    
+            if key == key_down and player_moveable(x, y + 1):
+                move_player(x, y + 1) 
+            if key == key_left and player_moveable(x - 1, y):
+                move_player(x - 1, y) 
+            if key == key_right and player_moveable(x + 1, y):
+                move_player(x + 1, y) 
             if key == 'q':
-                sokoban_exit()
+                exit()
+            sokoban_render_map(level_map)
+        # kontrollera om level är klar
 
-def sokoban_exit():
-    """ exits the game """
-    sys.exit() # clean kill of application
+def find_player(x=0, y=0):
+    """ locate the player on the board, returns x, y and current player sprite """
+    for row in level_map:
+        for item in row:
+            if item[1] == player_normal or item[1] == player_on_storage:
+                x = item[0]
+                y = level_map.index(row)
+                player = item[1]
+    return [x, y, player]
 
-def helper_debug_l(data):
-    for item in data: 
-        print(item)
+def player_moveable(dx, dy):
+    """ is the target a wall """
+    x, y, player = find_player()
+    moveable = False 
+    return True
+    try:
+        if level_map[dy][dx][1] != wall:
+            return True   
+        elif level_map[dy][dx][1] == wall: 
+            return False 
+    except: 
+        return True
 
-def helper_debug_s(string): 
-    print(string)
-
-def helper_sort(level_map=level_map):  
-    """ sort the level on x and y cords """
-    temp = []
-    for item in sorted(level_map, key = lambda x : (x[1], x[0])): # sort by second arg in current item  
-        temp += item[2]   
-    return temp 
-
-def find_player():
-    """ locate the player on the board """
-    for row in level_map: 
-        x = 0 
-        y = 0
-                       
-    return x ,y
-
-def moveable(dx, dy):
-    return true
-    
-def move(dx, dy):
-    pass
-
-def helper_move(dx, dy):
-    """ move objects to new location """
-    pass
-
-def helper_moveable(dx,dy): 
-    """ can move to location """
-    pass
-
-def helper_getmove(level, moves): 
-    """ input player move or quit command """
-    str_help = 'level:{level} / moves:{moves} $: '
-    keys = input(str_help.format(level=level + 1,moves=moves))
-    keys = list(keys.lower())
-    return keys
-
-def helper_clear_screen():
+def move_player(dx, dy):
+    """ moves a player to a new position and deletes the old """
+    x, y, player = find_player()
+    level_map[dy].remove([x, player]) 
+    for item in level_map[dy]:
+        if item[0] > dx:
+            level_map[dy].insert(level_map[dy].index(item), [dx, player])
+            break
+        
+def clear_screen():
     """ clear the screen """
     os.system("clear")
     return
+
+def exit():
+    """ exits the game """
+    sys.exit() # clean kill of application
 
 def main():
     sokoban_levels()                        # loads leveles into list
